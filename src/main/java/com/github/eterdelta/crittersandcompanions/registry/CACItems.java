0ba68f1786540b309e22de727c20bd87cd5133a5
LLytho <main@lytho.dev>
2022-11-05 22:40:26 +0100

    Add spawn eggs back

--- src/main/java/com/github/eterdelta/crittersandcompanions/registry/CACItems.java
+++ src/main/java/com/github/eterdelta/crittersandcompanions/registry/CACItems.java
@@ -10,6 +10,7 @@ import net.minecraft.world.food.Foods;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.MobBucketItem;
+import net.minecraft.world.item.SpawnEggItem;
 import net.minecraft.world.level.material.Fluids;
 
 public class CACItems {
@@ -28,12 +29,12 @@ public class CACItems {
     public static final RegistryObject<Item> SEA_BUNNY_BUCKET = ITEMS.register("sea_bunny_bucket", () -> new MobBucketItem(CACEntities.SEA_BUNNY.get(), Fluids.WATER, SoundEvents.BUCKET_EMPTY_FISH, (new Item.Properties()).stacksTo(1).tab(CrittersAndCompanions.CREATIVE_TAB)));
 
     // TODO spawn eggs
-//    public static final RegistryObject<Item> DRAGONFLY_SPAWN_EGG = ITEMS.register("dragonfly_spawn_egg", () -> new ForgeSpawnEggItem(CACEntities.DRAGONFLY, 0x08EECF, 0xD3FF96, (new Item.Properties()).tab(CrittersAndCompanions.CREATIVE_TAB)));
-//    public static final RegistryObject<Item> FERRET_SPAWN_EGG = ITEMS.register("ferret_spawn_egg", () -> new ForgeSpawnEggItem(CACEntities.FERRET, 0xC5AC88, 0x37212D, (new Item.Properties()).tab(CrittersAndCompanions.CREATIVE_TAB)));
-//    public static final RegistryObject<Item> DUMBO_OCTOPUS_SPAWN_EGG = ITEMS.register("dumbo_octopus_spawn_egg", () -> new ForgeSpawnEggItem(CACEntities.DUMBO_OCTOPUS, 0xFCDC4C, 0x162630, (new Item.Properties()).tab(CrittersAndCompanions.CREATIVE_TAB)));
-//    public static final RegistryObject<Item> KOI_FISH_SPAWN_EGG = ITEMS.register("koi_fish_spawn_egg", () -> new ForgeSpawnEggItem(CACEntities.KOI_FISH, 0xF3ECED, 0xFB5321, (new Item.Properties()).tab(CrittersAndCompanions.CREATIVE_TAB)));
-//    public static final RegistryObject<Item> LEAF_INSECT_SPAWN_EGG = ITEMS.register("leaf_insect_spawn_egg", () -> new ForgeSpawnEggItem(CACEntities.LEAF_INSECT, 0xDAD475, 0x3C6C34, (new Item.Properties()).tab(CrittersAndCompanions.CREATIVE_TAB)));
-//    public static final RegistryObject<Item> OTTER_SPAWN_EGG = ITEMS.register("otter_spawn_egg", () -> new ForgeSpawnEggItem(CACEntities.OTTER, 0x352C34, 0xB49494, (new Item.Properties()).tab(CrittersAndCompanions.CREATIVE_TAB)));
-//    public static final RegistryObject<Item> RED_PANDA_SPAWN_EGG = ITEMS.register("red_panda_spawn_egg", () -> new ForgeSpawnEggItem(CACEntities.RED_PANDA, 0xF4943C, 0x13131B, (new Item.Properties()).tab(CrittersAndCompanions.CREATIVE_TAB)));
-//    public static final RegistryObject<Item> SEA_BUNNY_SPAWN_EGG = ITEMS.register("sea_bunny_spawn_egg", () -> new ForgeSpawnEggItem(CACEntities.SEA_BUNNY, 0xF4ECE4, 0x453337, (new Item.Properties()).tab(CrittersAndCompanions.CREATIVE_TAB)));
+    public static final RegistryObject<Item> DRAGONFLY_SPAWN_EGG = ITEMS.register("dragonfly_spawn_egg", () -> new SpawnEggItem(CACEntities.DRAGONFLY.get(), 0x08EECF, 0xD3FF96, (new Item.Properties()).tab(CrittersAndCompanions.CREATIVE_TAB)));
+    public static final RegistryObject<Item> FERRET_SPAWN_EGG = ITEMS.register("ferret_spawn_egg", () -> new SpawnEggItem(CACEntities.FERRET.get(), 0xC5AC88, 0x37212D, (new Item.Properties()).tab(CrittersAndCompanions.CREATIVE_TAB)));
+    public static final RegistryObject<Item> DUMBO_OCTOPUS_SPAWN_EGG = ITEMS.register("dumbo_octopus_spawn_egg", () -> new SpawnEggItem(CACEntities.DUMBO_OCTOPUS.get(), 0xFCDC4C, 0x162630, (new Item.Properties()).tab(CrittersAndCompanions.CREATIVE_TAB)));
+    public static final RegistryObject<Item> KOI_FISH_SPAWN_EGG = ITEMS.register("koi_fish_spawn_egg", () -> new SpawnEggItem(CACEntities.KOI_FISH.get(), 0xF3ECED, 0xFB5321, (new Item.Properties()).tab(CrittersAndCompanions.CREATIVE_TAB)));
+    public static final RegistryObject<Item> LEAF_INSECT_SPAWN_EGG = ITEMS.register("leaf_insect_spawn_egg", () -> new SpawnEggItem(CACEntities.LEAF_INSECT.get(), 0xDAD475, 0x3C6C34, (new Item.Properties()).tab(CrittersAndCompanions.CREATIVE_TAB)));
+    public static final RegistryObject<Item> OTTER_SPAWN_EGG = ITEMS.register("otter_spawn_egg", () -> new SpawnEggItem(CACEntities.OTTER.get(), 0x352C34, 0xB49494, (new Item.Properties()).tab(CrittersAndCompanions.CREATIVE_TAB)));
+    public static final RegistryObject<Item> RED_PANDA_SPAWN_EGG = ITEMS.register("red_panda_spawn_egg", () -> new SpawnEggItem(CACEntities.RED_PANDA.get(), 0xF4943C, 0x13131B, (new Item.Properties()).tab(CrittersAndCompanions.CREATIVE_TAB)));
+    public static final RegistryObject<Item> SEA_BUNNY_SPAWN_EGG = ITEMS.register("sea_bunny_spawn_egg", () -> new SpawnEggItem(CACEntities.SEA_BUNNY.get(), 0xF4ECE4, 0x453337, (new Item.Properties()).tab(CrittersAndCompanions.CREATIVE_TAB)));
 }
