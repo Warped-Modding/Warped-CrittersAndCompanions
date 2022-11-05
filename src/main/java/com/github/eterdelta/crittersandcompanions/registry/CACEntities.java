84db0496662543f4038f21327f69f7736eaed363
LLytho <main@lytho.dev>
2022-11-05 23:10:57 +0100

    Use FabricEntityTypeBuilder

--- src/main/java/com/github/eterdelta/crittersandcompanions/registry/CACEntities.java
+++ src/main/java/com/github/eterdelta/crittersandcompanions/registry/CACEntities.java
@@ -4,19 +4,26 @@ import com.github.eterdelta.crittersandcompanions.CrittersAndCompanions;
 import com.github.eterdelta.crittersandcompanions.entity.*;
 import com.github.eterdelta.crittersandcompanions.platform.DeferredRegister;
 import com.github.eterdelta.crittersandcompanions.platform.RegistryObject;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.core.Registry;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 
 public class CACEntities {
     public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(Registry.ENTITY_TYPE, CrittersAndCompanions.MODID);
 
-    public static final RegistryObject<EntityType<DragonflyEntity>> DRAGONFLY = ENTITIES.register("dragonfly", () -> EntityType.Builder.of(DragonflyEntity::new, MobCategory.AMBIENT).sized(0.8F, 0.2F).build("dragonfly"));
-    public static final RegistryObject<EntityType<DumboOctopusEntity>> DUMBO_OCTOPUS = ENTITIES.register("dumbo_octopus", () -> EntityType.Builder.of(DumboOctopusEntity::new, MobCategory.CREATURE).sized(0.4F, 0.4F).build("dumbo_octopus"));
-    public static final RegistryObject<EntityType<FerretEntity>> FERRET = ENTITIES.register("ferret", () -> EntityType.Builder.of(FerretEntity::new, MobCategory.CREATURE).sized(0.75F, 0.65F).build("ferret"));
-    public static final RegistryObject<EntityType<KoiFishEntity>> KOI_FISH = ENTITIES.register("koi_fish", () -> EntityType.Builder.of(KoiFishEntity::new, MobCategory.WATER_AMBIENT).sized(0.6F, 0.3F).build("koi_fish"));
-    public static final RegistryObject<EntityType<LeafInsectEntity>> LEAF_INSECT = ENTITIES.register("leaf_insect", () -> EntityType.Builder.of(LeafInsectEntity::new, MobCategory.AMBIENT).sized(0.4F, 0.3F).build("leaf_insect"));
-    public static final RegistryObject<EntityType<OtterEntity>> OTTER = ENTITIES.register("otter", () -> EntityType.Builder.of(OtterEntity::new, MobCategory.WATER_CREATURE).sized(0.8F, 0.4F).build("otter"));
-    public static final RegistryObject<EntityType<RedPandaEntity>> RED_PANDA = ENTITIES.register("red_panda", () -> EntityType.Builder.of(RedPandaEntity::new, MobCategory.CREATURE).sized(0.65F, 0.55F).build("red_panda"));
-    public static final RegistryObject<EntityType<SeaBunnyEntity>> SEA_BUNNY = ENTITIES.register("sea_bunny", () -> EntityType.Builder.of(SeaBunnyEntity::new, MobCategory.WATER_AMBIENT).sized(0.45F, 0.3F).build("sea_bunny"));
+    public static final RegistryObject<EntityType<DragonflyEntity>> DRAGONFLY = ENTITIES.register("dragonfly", () -> create(DragonflyEntity::new, MobCategory.AMBIENT, 0.8F, 0.2F));
+    public static final RegistryObject<EntityType<DumboOctopusEntity>> DUMBO_OCTOPUS = ENTITIES.register("dumbo_octopus", () -> create(DumboOctopusEntity::new, MobCategory.CREATURE, 0.4F, 0.4F));
+    public static final RegistryObject<EntityType<FerretEntity>> FERRET = ENTITIES.register("ferret", () -> create(FerretEntity::new, MobCategory.CREATURE, 0.75F, 0.65F));
+    public static final RegistryObject<EntityType<KoiFishEntity>> KOI_FISH = ENTITIES.register("koi_fish", () -> create(KoiFishEntity::new, MobCategory.WATER_AMBIENT, 0.6F, 0.3F));
+    public static final RegistryObject<EntityType<LeafInsectEntity>> LEAF_INSECT = ENTITIES.register("leaf_insect", () -> create(LeafInsectEntity::new, MobCategory.AMBIENT, 0.4F, 0.3F));
+    public static final RegistryObject<EntityType<OtterEntity>> OTTER = ENTITIES.register("otter", () -> create(OtterEntity::new, MobCategory.WATER_CREATURE, 0.8F, 0.4F));
+    public static final RegistryObject<EntityType<RedPandaEntity>> RED_PANDA = ENTITIES.register("red_panda", () -> create(RedPandaEntity::new, MobCategory.CREATURE, 0.65F, 0.55F));
+    public static final RegistryObject<EntityType<SeaBunnyEntity>> SEA_BUNNY = ENTITIES.register("sea_bunny", () -> create(SeaBunnyEntity::new, MobCategory.WATER_AMBIENT, 0.45F, 0.3F));
+
+    private static <E extends Entity> EntityType<E> create(EntityType.EntityFactory<E> entityFactory, MobCategory category, float width, float height) {
+        return FabricEntityTypeBuilder.create(category, entityFactory).dimensions(EntityDimensions.scalable(width, height)).build();
+    }
 }
