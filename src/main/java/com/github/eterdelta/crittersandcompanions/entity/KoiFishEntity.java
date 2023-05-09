666e687e8a20046d3af0b11316fc33862cbb5e55
ZekeZ <contact@zekez.dev>
2023-05-09 17:06:14 +1000

    update KoiFish

--- src/main/java/com/github/eterdelta/crittersandcompanions/entity/KoiFishEntity.java
+++ src/main/java/com/github/eterdelta/crittersandcompanions/entity/KoiFishEntity.java
@@ -39,7 +39,7 @@ public class KoiFishEntity extends AbstractSchoolingFish implements IAnimatable 
     }
 
     public static AttributeSupplier.Builder createAttributes() {
-        return Mob.createMobAttributes().add(Attributes.MAX_HEALTH, 8.0D).add(Attributes.MOVEMENT_SPEED, 0.1D);
+        return Mob.createMobAttributes().add(Attributes.MAX_HEALTH, 3.0D).add(Attributes.MOVEMENT_SPEED, 0.1D);
     }
 
     @Override
